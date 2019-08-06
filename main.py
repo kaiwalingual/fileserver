b12cd8af2a7f4f03328ba4dae2dc4594838d71c5
Shogo Takata <pineapplehunter.daniel@gmail.com>
2019-08-06 16:52:48 +0900

    gdrive version

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from datetime import datetime
 from pathlib import Path
 from PIL import Image
 from io import BytesIO
+import subprocess
+import os
+
+from tokens import GDRIVE_PARENT_DIR
 
 base_dir = Path("pics")
 
@@ -22,7 +26,17 @@ def index(name=None, minname=None):
         base_dir.mkdir(exist_ok=True)
         f = request.files["file"]
         fname = datetime.now().strftime("%y%m%d%H%M%S") + ".jpg"
-        f.save(base_dir / fname)
+        with open(base_dir / fname, "wb") as out:
+            f.save(out)
+
+        proc = subprocess.run(["gdrive", "upload", "-p", GDRIVE_PARENT_DIR, base_dir / fname],
+                              stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        driveid = proc.stdout.decode("utf8").split("\n")[1].split(" ")[1]
+
+        with open(base_dir / (fname + ".txt"), "w") as f:
+            f.write(driveid)
+
+        os.remove(base_dir / fname)
 
         return fname
     if minname is not None:
@@ -31,16 +45,30 @@ def index(name=None, minname=None):
     if name is None:
         return "specify a name at /<name>.jpg"
 
-    path = base_dir / name
+    path = base_dir / (name + ".txt")
     if path.exists():
+        gdriveid = ""
+        with open(path) as f:
+            gdriveid = f.readline()
+        subprocess.run(["gdrive", "download", gdriveid], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
         if minname is not None:
-            im = Image.open(base_dir / name)
+            im = Image.open(name)
+            im = im.convert("RGB")
             im.thumbnail((240, 240))
             im_io = BytesIO()
             im.save(im_io, "JPEG")
             im_io.seek(0)
+            os.remove(name)
             return send_file(im_io, mimetype="image/jpeg")
-        return send_file(path, mimetype="image/jpeg")
+
+        im = Image.open(name)
+        im = im.convert("RGB")
+        im_io = BytesIO()
+        im.save(im_io, "JPEG")
+        im_io.seek(0)
+        os.remove(name)
+        return send_file(im_io, mimetype="image/jpeg")
     else:
         return "NO", 404
 
